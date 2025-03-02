b34bab6b763d908fd604035f57e32b4939863867
Mauro Wencelewski <mauro.wencelewski@gmail.com>
2025-03-02 20:39:21 -0300

    Updating migrations

--- app/migrations/versions/49d7ef6c483d_removing_move_detais_from_move_version_.py
+++ app/migrations/versions/49d7ef6c483d_removing_move_detais_from_move_version_.py
@@ -9,7 +9,7 @@ from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
-
+import sqlmodel
 
 # revision identifiers, used by Alembic.
 revision: str = '49d7ef6c483d'
--- app/migrations/versions/502699fa3736_pre_production_tables.py
+++ app/migrations/versions/502699fa3736_pre_production_tables.py
@@ -9,7 +9,7 @@ from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
-
+import sqlmodel
 
 # revision identifiers, used by Alembic.
 revision: str = '502699fa3736'
